199a07ac6f2f61b45bf447e42230297a519f6b02
Isaac Dubrawsky <60890318+isiic12@users.noreply.github.com>
2020-09-24 18:26:41 -0400

    Finished tests for BST class
    Change-Id: I62bb7ab3e150c515475f0802439037b63f56696c

--- Implemented Data Structures/src/binarysearchtree/BST.java
+++ Implemented Data Structures/src/binarysearchtree/BST.java
@@ -1,32 +1,67 @@
 package binarysearchtree;
 
+import java.util.Map;
+import java.util.Set;
+
 public class BST<K extends Comparable<K>, V> {
 	
-	public Node root;
+	private Node root;
 
-	public class Node{
-		public K key;
-		public V val;
-		public Node left, right;
+	protected class Node{
+		protected K key;
+		protected V val;
+		protected Node left, right;
 		
 		Node(K key, V value) {
 			this.key = key;
 			this.val = value;
 		}
 		
+		@Override
 		public String toString() {
-			return this.key + ":" + this.val;
+			return this.key + ":" + this.val + " ";
 		}
 	}
 	
+	/**
+	 * Default constructor; initializes an empty tree with a null root
+	 */
 	public BST() {
 		this.root = null;
 	}
 	
+	/**
+	 * Constructor for initializing tree with given key-value pair
+	 * @param key Key of root node
+	 * @param value Value of root node
+	 */
 	public BST(K key, V value) {
 		this.root = new Node(key, value);
 	}
 	
+	/**
+	 * Constructor that allows user to create a new tree and populate it
+	 * using key-value pairs stored in a map. Due to the nature of maps,
+	 * this will not guarantee any particular ordering of elements within the
+	 * tree, outside of normal tree orderings - i.e. multiple instantiations 
+	 * of trees based on the same map may have different roots and shapes.
+	 * 
+	 * @param map Map whose key-value pairs will be used to populate a new tree
+	 */
+	public BST(Map<K, V> map) {
+		this();
+		if (!map.isEmpty()) {
+			Set<K> keys = map.keySet();
+			for (K key : keys) {
+				this.insert(key, map.get(key));
+			}
+		}
+	}
+	
+	/**
+	 * Getter for tree's root
+	 * @return Root node of tree
+	 */
 	public Node getRoot() {
 		return this.root;
 	}
@@ -48,29 +83,43 @@ public class BST<K extends Comparable<K>, V> {
 	private Node insert(Node n, K key, V value) {
 		if (n == null)
 			n = new Node(key, value);
-		else if (key.compareTo(n.key) < 0)
+		
+		int comparison = key.compareTo(n.key);
+		if (comparison < 0)
 			n.left = insert(n.left, key, value);
-		else if (key.compareTo(n.key) > 0)
+		else if (comparison > 0)
 			n.right = insert(n.right, key, value);
 		else
 			n.val = value;
 		return n;
 	}
 	
+	/**
+	 * Checks if a node with the given key exists in the tree
+	 * @param key Key to search for in the tree
+	 * @return True if node with the given key exists in the tree, false otherwise
+	 */
 	public boolean containsKey(K key) {
 		return containsKey(root, key);
 	}
 	
 	private boolean containsKey(Node n, K key) {
 		if (n == null) return false;
-		if (key.compareTo(n.key) < 0)
+		
+		int comparison = key.compareTo(n.key);
+		if (comparison < 0)
 			return containsKey(n.left, key);
-		else if (key.compareTo(n.key) > 0)
+		else if (comparison > 0)
 			return containsKey(n.right, key);
 		else
 			return true;
 	}
 	
+	/**
+	 * Checks if a node with a given value exists in the tree
+	 * @param value Value to search for in the tree
+	 * @return True if node with the given value exists in the tree, false otherwise
+	 */
 	public boolean containsValue(V value) {
 		return containsValue(root, value);
 	}
@@ -97,9 +146,11 @@ public class BST<K extends Comparable<K>, V> {
 	 */
 	private V get(Node n, K key) {
 		if (n == null) return null;
-		if (key.compareTo(n.key) < 0)
+		
+		int comparison = key.compareTo(n.key);
+		if (comparison < 0)
 			return get(n.left, key);
-		else if (key.compareTo(n.key) > 0)
+		else if (comparison > 0)
 			return get(n.right, key);
 		return n.val;
 	}
@@ -115,52 +166,21 @@ public class BST<K extends Comparable<K>, V> {
 	private Node delete(Node n, K key) {
 		if (n == null) return null;
 		
-		if (key.compareTo(n.key) < 0)
+		int comparison = key.compareTo(n.key);
+		if (comparison < 0)
 			n.left = delete(n.left, key);
-		else if (key.compareTo(n.key) > 0)
+		else if (comparison > 0)
 			n.right = delete(n.right, key);
 		else {
-			if (n.left == null && n.right == null) {
-				n = null;
-				return n;
-			}
-			Node temp;
-			if (n.right != null)
-				temp = min(n.right);
-			else
-				temp = max(n.left);
+			if (n.right == null) return n.left;
+			if (n.left == null)  return n.right;
 			
+			Node temp = min(n.right);
 			delete(root, temp.key);
 			n.key = temp.key;
 			n.val = temp.val;
 		}
 		return n;
-	}
-	
-	/**
-	 * Method for getting the maximum key in the tree
-	 * @return Maximum key in the tree
-	 */
-	public K maxKey() {
-		return maxKey(root);
-	}
-	
-	/*Helper function returns the maximum key from a given node*/
-	private K maxKey(Node n) {
-		Node temp = max(root);
-		if (temp != null) 	return temp.key;
-		return null;
-	}
-	
-	/**
-	 * Method for getting the value associated with the maximum key in the tree.
-	 * @return Value associated to maximum key in the tree.
-	 */
-	public V maxVal() {
-		Node n = max(root);
-		if (n != null)
-			return n.val;
-		return null;
 	}
 	
 	/* Finds the maximum node in the tree by key by going as far right as possible in 
@@ -173,29 +193,25 @@ public class BST<K extends Comparable<K>, V> {
 	}
 	
 	/**
-	 * Public method for getting the minimum key in the tree
-	 * @return Minimum key in the tree
+	 * Method for getting the maximum key in the tree
+	 * @return Maximum key in the tree
 	 */
-	public K minKey() {
-		return minKey(root);
-	}
-	
-	/*Helper function returns the minimum key from a given node*/
-	private K minKey(Node n) {
-		Node temp = min(root);
-		if (temp != null)  return temp.key;
-		return null;
+	public K maxKey() {
+		Node temp = max(root);
+		if (temp == null) return null;
+		return temp.key;
 	}
 	
 	/**
-	 * Method for getting the value associated with the minimum key in the tree.
-	 * @return Value associated to minimum key in the tree.
+	 * Method for getting the value associated with the maximum key in the tree.
+	 * @return Value associated to maximum key in the tree.
 	 */
-	public V minVal() {
-		Node n = min(root);
-		if (n != null)  return n.val;
-		return null;
+	public V maxVal() {
+		Node temp = max(root);
+		if (temp == null) return null;
+		return temp.val;
 	}
+	
 	
 	/* Find the minimum node in the tree by key by going as far left as possible in
 	 * the tree. IF the tree's root is null (tree is empty) returns null.*/
@@ -207,25 +223,71 @@ public class BST<K extends Comparable<K>, V> {
 	}
 	
 	/**
+	 * Public method for getting the minimum key in the tree
+	 * @return Minimum key in the tree
+	 */
+	public K minKey() {
+		Node temp = min(root);
+		if (temp == null) return null;
+		return temp.key;
+	}
+	
+	/**
+	 * Method for getting the value associated with the minimum key in the tree.
+	 * @return Value associated to minimum key in the tree.
+	 */
+	public V minVal() {
+		Node temp = min(root);
+		if (temp == null) return null;
+		return temp.val;
+	}
+	
+	/**
+	 * Method inverts the tree (turns the tree into a mirror image of itself).
+	 * Result is no longer valid as a binary search tree due to violation of node ordering.
+	 */
+	public void invert() {
+		invert(root);
+	}
+	
+	private Node invert(Node n) {
+		if (n == null) return null;
+		
+		Node left = n.left;
+		Node right = n.right;
+		
+		invert(n.left);
+		invert(n.right);
+		
+		n.left = right;
+		n.right = left;
+		
+		return n;
+	}
+	
+	/**
 	 * Verifies that tree is a valid BST.
 	 * @return True if tree is a valid BST, false otherwise.
 	 */
 	public boolean isBST() {
-		K min = minKey(root);
-		K max = maxKey(root);
+		K min = minKey();
+		K max = maxKey();
 		return isBST(root, min, max);
 	}
 	
 	private boolean isBST(Node n, K min, K max) {
 		if (n == null) return true;
-		if (min != null && n.key.compareTo(min) < 0) return false;
-		if (max != null && n.key.compareTo(max) > 0) return false;
-		return isBST(n.left, min, max) && isBST(n.right, min, max);
+		
+		int cmpMin = n.key.compareTo(min);
+		int cmpMax = n.key.compareTo(max);
+		if (cmpMin < 0 || cmpMax > 0) return false;
+		
+		return isBST(n.left, min, n.key) && isBST(n.right, n.key, max);
 	}
 	
 	/**
-	 * Checks if the BST is a full tree.
-	 * @return True if all nodes have 0 or 2 children, false if any node has only 1 child.
+	 * Checks if the bst is a full tree (all nodes have either 0 or 2 children)
+	 * @return True if the tree is full, false otherwise
 	 */
 	public boolean isFull() {
 		return isFull(root);
@@ -240,6 +302,11 @@ public class BST<K extends Comparable<K>, V> {
 		return false;
 	}
 	
+	/**
+	 * Checks if the bst is complete (all levels but the last are entirely 
+	 * filled, and the nodes in the last level are as far left as possible)
+	 * @return True if tree is complete, false otherwise
+	 */
 	public boolean isComplete() {
 		int size = size(root);
 		return isComplete(root, 0, size);
@@ -253,76 +320,87 @@ public class BST<K extends Comparable<K>, V> {
 	}
 	
 	/**
-	 * Prints the preOrder traversal of the tree
+	 * Performs a pre-order (root-left-right) traversal of the tree
+	 * @return String of tree in pre-order
 	 */
-	public void preOrder() {
-		if (root == null) {
-			System.out.println("Root is null");
-			return;
-		}
-		preOrder(root);
-		System.out.println();
+	public String preOrder() {
+		if (this.root == null) return "";
+		String output = preOrder(root);
+		return output.substring(0, output.length() - 1);
 	}
 	
-	private void preOrder(Node n) {
-		if (n == null) return;
-		System.out.print(n + " ");
-		preOrder(n.left);
-		preOrder(n.right);
+	private String preOrder(Node n) {
+		if (n == null) return "";
+		
+		String output = "";
+		output += n.toString();
+		output += preOrder(n.left);
+		output += preOrder(n.right);
+		
+		return output;
 	}
 	
 	/**
-	 * Prints the inOrder traversal of the tree.
+	 * Performs an in-order (left-root-right) traversal of the tree
+	 * @return String of tree in in-order
 	 */
-	public void inOrder() {
-		if (root == null) {
-			System.out.println("Root is null");
-			return;
-		}
-		inOrder(root);
-		System.out.println();
+	public String inOrder() {
+		if (this.root == null) return "";
+		String output = inOrder(root);
+		return output.substring(0, output.length() - 1);
 	}
 	
-	private void inOrder(Node n) {
-		if (n == null) return;
-		inOrder(n.left);
-		System.out.print(n + " ");
-		inOrder(n.right);
-	}
-	
-	public void reverseOrder() {
-		if (root == null) {
-			System.out.println("Root is null");
-			return;
-		}
-		reverseOrder(root);
-		System.out.println();
-	}
-	
-	private void reverseOrder(Node n) {
-		if (n == null) return;
-		reverseOrder(n.right);
-		System.out.print(n + " ");
-		reverseOrder(n.left);
+	private String inOrder(Node n) {
+		if (n == null) return "";
+		
+		String output = "";
+		output += inOrder(n.left);
+		output += n.toString();
+		output += inOrder(n.right);
+		
+		return output;
 	}
 	
 	/**
-	 * Prints the postOrder traversal of the tree.
+	 * Performs a post-order (left-right-root) traversal of the tree
+	 * @return String of tree in post-order
 	 */
-	public void postOrder() {
-		if (root == null) {
-			System.out.println("Root is null");
-			return;
-		}
-		postOrder(root);
-		System.out.println();
+	public String postOrder() {
+		if (this.root == null) return "";
+		String output = postOrder(root);
+		return output.substring(0, output.length() - 1);
 	}
 	
-	private void postOrder(Node n) {
-		if (n == null) return;
-		postOrder(n.left);
-		postOrder(n.right);
-		System.out.print(n + " ");
+	private String postOrder(Node n) {
+		if (n == null) return "";
+		
+		String output = "";
+		output += postOrder(n.left);
+		output += postOrder(n.right);
+		output += n.toString();
+		
+		return output;
+	}
+	
+	/**
+	 * Performs a reverse-order (right-root-left) traversal of the tree
+	 * @return String of tree in reverse-order
+	 */
+	public String reverseOrder() {
+		if (this.root == null) return "";
+		String output = reverseOrder(root);
+		return output.substring(0, output.length() - 1);
+	}
+	
+	private String reverseOrder(Node n) {
+		if (n == null) return ""; 
+		
+		String output = "";
+		output += reverseOrder(n.right);
+		output += n.toString();
+		output += reverseOrder(n.left);
+		
+		return output;
 	}
 	
 	/**
@@ -349,7 +427,7 @@ public class BST<K extends Comparable<K>, V> {
 	
 	private int minHeight(Node n) {
 		if (n == null) return 0;
-		return 1 + Math.min(minHeight(n.left), minHeight(n.right));
+		return (1 + Math.min(minHeight(n.left), minHeight(n.right)));
 	}
 	
 	/**
@@ -377,6 +455,20 @@ public class BST<K extends Comparable<K>, V> {
 		if (n == null) return 0;
 		if (n.left == null && n.right == null) return 1;
 		return numLeaves(n.left) + numLeaves(n.right);
+	}
+	
+	/**
+	 * Gets the number of internal nodes in the tree
+	 * @return Number of nodes in the tree having at least one child
+	 */
+	public int numInternal() {
+		return numInternal(root);
+	}
+	
+	private int numInternal(Node n) {
+		if (n == null) 						   return 0;
+		if (n.left == null && n.right == null) return 0;
+		return 1 + (numInternal(n.left) + numInternal(n.right));
 	}
 	
 	/**
--- Implemented Data Structures/src/binarysearchtree/tests.java
+++ Implemented Data Structures/src/binarysearchtree/tests.java
@@ -1,11 +1,27 @@
 package binarysearchtree;
 
 import static org.junit.Assert.*;
-import java.util.*;
+import java.util.HashMap;
 import org.junit.Test;
 
 public class tests {
 	
+	//Method to quickly create a new binary search tree for many tests
+	public BST<Integer, String> createBST() {
+		BST<Integer, String> bst = new BST<Integer, String>();
+		
+		bst.insert(50, "Fifty");
+		bst.insert(30, "Thirty");
+		bst.insert(20, "Twenty");
+		bst.insert(40, "Fourty");
+		bst.insert(70, "Seventy");
+		bst.insert(60, "Sixty");
+		bst.insert(80, "Eighty");
+		
+		return bst;
+	}
+	
+	//Tests insertion to a tree and the size method
 	@Test
 	public void testInsertAndSize() {
 		BST<Integer, String> bst = new BST<Integer, String>();
@@ -25,117 +41,401 @@ public class tests {
 		assertEquals(7, bst.size());
 	}
 
+	//Tests the containsKey and containsValue methods for proper true and false evaluations
 	@Test
 	public void testContains() {
-		BST<Integer, String> bst = new BST<Integer, String>();
-		
-		bst.insert(20, "Twenty");
-		bst.insert(50, "Fifty");
-		bst.insert(10, "Ten");	
-		bst.insert(30, "Thirty");
-		bst.insert(15, "Fifteen");
+		BST<Integer, String> bst = createBST();
 		
 		assertTrue(bst.containsKey(20));
-		assertTrue(bst.containsKey(30));
-		assertTrue(bst.containsKey(15));
+		assertTrue(bst.containsKey(40));
+		assertTrue(bst.containsKey(50));
+		assertTrue(bst.containsKey(80));
 		
-		assertFalse(bst.containsKey(80));
+		assertFalse(bst.containsKey(90));
 		assertFalse(bst.containsKey(0));
 		
-		assertTrue(bst.containsValue("Twenty"));
-		assertTrue(bst.containsValue("Fifty"));
-		assertTrue(bst.containsValue("Fifteen"));
+		assertTrue(bst.containsValue("Thirty"));
+		assertTrue(bst.containsValue("Sixty"));
+		assertTrue(bst.containsValue("Seventy"));
 		
-		assertFalse(bst.containsValue("Eighty"));
+		assertFalse(bst.containsValue("Ten"));
 		assertFalse(bst.containsValue("Zero"));
 	}
 	
+	//Tests proper deleting of leaf nodes
 	@Test
 	public void testDeleteLeaves() {
-		BST<Integer, String> bst = new BST<Integer, String>();
+		BST<Integer, String> bst = createBST();
 		
-		bst.insert(20, "Twenty");
-		bst.insert(10, "Ten");
-		bst.insert(30, "Thirty");
-		
+		bst.delete(80);
 		bst.delete(20);
-		bst.inOrder();
+		
+		String traversal = bst.inOrder();
+		String correct = "30:Thirty 40:Fourty 50:Fifty 60:Sixty 70:Seventy";
+		assertEquals(correct, traversal);
+		
+		bst.delete(40);
+		bst.delete(60);
+		
+		traversal = bst.inOrder();
+		correct = "30:Thirty 50:Fifty 70:Seventy";
+		assertEquals(correct, traversal);
+	}
+	
+	//Tests proper deleting of internal nodes (have at least one child)
+	@Test
+	public void testDeleteInternal1() {
+		BST<Integer, String> bst = createBST();
 		
 		bst.delete(30);
-		bst.inOrder();
 		
-		bst.delete(10);
-		bst.inOrder();
+		String traversal = bst.inOrder();
+		String correct = "20:Twenty 40:Fourty 50:Fifty 60:Sixty 70:Seventy 80:Eighty";
+		assertEquals(correct, traversal);
+		
+		bst.insert(90, "Ninety");
+		bst.insert(75, "Seventy-Five");
+		bst.delete(70);
+		
+		traversal = bst.inOrder();
+		correct = "20:Twenty 40:Fourty 50:Fifty 60:Sixty 75:Seventy-Five 80:Eighty 90:Ninety";
+		assertEquals(correct, traversal);
 	}
 	
+	//Another test of proper deleting of internal nodes
 	@Test
-	public void testDeleteInternal() {
-		BST<Integer, String> bst = new BST<Integer, String>();
-	}
-	
-	/*
-	@Test
-	public void testInOrder() {
-		BST<Integer, String> bst = new BST<Integer, String>();
-		
-		bst.insert(50, "Fifty");
-		bst.insert(20, "Twenty");
-		bst.insert(10, "Ten");	
-		bst.insert(30, "Thirty");
-		bst.insert(70, "Seventy");
-		bst.insert(60, "Sixty");
-		bst.insert(80, "Eighty");
-		
-		System.out.print("\nIn Order: ");
-		bst.inOrder();
-	}
-	
-	@Test
-	public void testReverseOrder() {
+	public void testDeleteInternal2() {
 		BST<Integer, String> bst = new BST<Integer, String>();
 		
 		bst.insert(50, "Fifty");
-		bst.insert(20, "Twenty");
-		bst.insert(10, "Ten");	
-		bst.insert(30, "Thirty");
 		bst.insert(70, "Seventy");
 		bst.insert(60, "Sixty");
 		bst.insert(80, "Eighty");
+		bst.delete(70);
 		
-		System.out.print("\nReverse Order: ");
-		bst.reverseOrder();
+		String traversal = bst.preOrder();
+		String correct = "50:Fifty 80:Eighty 60:Sixty";
+		assertEquals(correct, traversal);
+		assertTrue(bst.getRoot().right.key == 80);
+		assertTrue(bst.getRoot().right.left.key == 60);
+		
+		bst.delete(80);
+		traversal = bst.reverseOrder();
+		correct = "60:Sixty 50:Fifty";
+		
+		assertEquals(correct, traversal);
+		assertTrue(bst.getRoot().right.key == 60);
+		assertNull(bst.getRoot().right.left);
+		assertNull(bst.getRoot().right.right);
 	}
 	
+	//Tests deleting of the root
+	@Test
+	public void testDeleteRoot() {
+		BST<Integer, String> bst = createBST();
+		bst.delete(50);
+
+		String traversal = bst.preOrder();
+		String correct = "60:Sixty 30:Thirty 20:Twenty 40:Fourty 70:Seventy 80:Eighty";
+		
+		assertEquals(correct, traversal);
+		assertTrue(bst.getRoot().key == 60);
+		assertTrue(bst.getRoot().right.key == 70);
+		assertTrue(bst.getRoot().left.key == 30);
+		
+		bst.delete(60);
+		traversal = bst.preOrder();
+		correct = "70:Seventy 30:Thirty 20:Twenty 40:Fourty 80:Eighty";
+		
+		assertEquals(correct, traversal);
+		assertTrue(bst.getRoot().key == 70);
+		assertTrue(bst.getRoot().right.key == 80);
+		assertTrue(bst.getRoot().left.key == 30);
+	}
+	
+	//Tests multiple deletions of nodes in the tree
+	@Test
+	public void testDeleteChain() {
+		BST<Integer, String> bst = new BST<Integer, String>();
+		bst.insert(50, "Fifty");
+		bst.insert(90, "Ninety");
+		bst.insert(80, "Eighty");
+		bst.insert(70, "Seventy");
+		bst.insert(60, "Sixty");
+		bst.delete(80);
+		
+		String traversal = bst.preOrder();
+		String correct = "50:Fifty 90:Ninety 70:Seventy 60:Sixty";
+
+		assertEquals(correct, traversal);
+	}
+
+	//Tests the max/min Key/Val methods for proper results
+	@Test
+	public void testMinMax() {
+		BST<Integer, String> bst = createBST();
+		
+		int maxKey = bst.maxKey();
+		int minKey = bst.minKey();
+		String maxVal = bst.maxVal();
+		String minVal = bst.minVal();
+		
+		assertEquals(80, maxKey);
+		assertEquals(20, minKey);
+		assertEquals("Eighty", maxVal);
+		assertEquals("Twenty", minVal);
+		
+		BST<Integer, String> bst2 = new BST<Integer, String>();
+		
+		assertNull(bst2.maxKey());
+		assertNull(bst2.minKey());
+		assertNull(bst2.maxVal());
+		assertNull(bst2.minVal());
+		
+		bst2.insert(50, "Fifty");
+		
+		assertTrue(bst2.maxKey() == 50);
+		assertTrue(bst2.maxKey() == bst2.minKey());
+		assertTrue(bst2.maxVal() == "Fifty");
+		assertTrue(bst2.maxVal() == bst2.minVal());
+	}
+
+	//Tests the get method for proper returned Values
+	@Test
+	public void testGet() {
+		BST<Integer, String> bst = createBST();
+		
+		String result1 = bst.get(50);
+		String result2 = bst.get(20);
+		String result3 = bst.get(80);
+		String result4 = bst.get(100);
+		String result5 = bst.get(-20);
+		
+		assertEquals("Fifty", result1);
+		assertEquals("Twenty", result2);
+		assertEquals("Eighty", result3);
+		assertNull(result4);
+		assertNull(result5);
+	}
+	
+	//Tests the height (max depth) and minHeight (min depth) of tree methods
+	@Test
+	public void testMaxAndMinHeights() {
+		BST<Integer, String> bst = createBST();
+		BST<Integer, String> bst2 = new BST<Integer, String>();
+		
+		assertEquals(bst.height(), bst.minHeight());
+		
+		bst.insert(90, "Ninety");
+		bst.insert(100, "One Hundred");
+		bst.delete(20);
+		bst.delete(40);
+		
+		assertEquals(5, bst.height());
+		assertEquals(2, bst.minHeight());
+		assertEquals(0, bst2.height());
+		assertEquals(0, bst2.minHeight());
+	}
+	
+	//Tests the clear method (resets the tree to be empty)
+	@Test
+	public void testClear() {
+		BST<Integer, String> bst = createBST();
+		
+		assertEquals(7, bst.size());
+		
+		bst.clear();
+		assertEquals(0, bst.size());
+		assertEquals(0, bst.height());
+		assertEquals(0, bst.minHeight());
+		assertNull(bst.getRoot());
+	}
+
+	//Tests the numLeaves method for proper counting of leaves
+	@Test
+	public void testNumLeaves() {
+		BST<Integer, String> bst = createBST();
+		
+		assertEquals(4, bst.numLeaves());
+
+		bst.delete(30);
+		bst.delete(60);		
+		assertEquals(2, bst.numLeaves());
+		
+		bst.clear();
+		assertEquals(0, bst.numLeaves());
+	}
+	
+	//Tests the numInternal method for proper counting of internal nodes
+	@Test
+	public void testNumInternal() {
+		BST<Integer, String> bst = createBST();
+		
+		assertEquals(3, bst.numInternal());
+		
+		bst.insert(90, "Ninety");
+		bst.insert(10, "Ten");
+		bst.insert(45, "Fourty-five");
+		assertEquals(6, bst.numInternal());
+		
+		bst.clear();		
+		assertEquals(0, bst.numInternal());
+	}
+	
+	//Tests the isFull method, which checks if all nodes in the tree have either 0 or 2 nodes
+	@Test
+	public void testIsFull() {
+		BST<Integer, String> bst = createBST();
+		
+		assertTrue(bst.isFull());
+		
+		bst.delete(20);
+		assertFalse(bst.isFull());
+		
+		bst.delete(40);
+		assertTrue(bst.isFull());
+		
+		bst.clear();
+		assertTrue(bst.isFull());
+	}
+	
+	//Tests the isComplete method, which checks if all levels but the last are filled out and the last level's nodes are left-justified
+	@Test
+	public void testIsComplete() {
+		BST<Integer, String> bst = createBST();
+		
+		assertTrue(bst.isComplete());
+		
+		bst.delete(80);
+		assertTrue(bst.isComplete());
+		
+		bst.delete(40);
+		assertFalse(bst.isComplete());
+		
+		bst.delete(60);
+		assertTrue(bst.isComplete());
+		
+		bst.delete(70);
+		assertFalse(bst.isComplete());
+		
+		bst.clear();
+		assertTrue(bst.isComplete());
+	}
+	
+	//Tests the isBST method, which checks if the tree has violated the proper ordering of nodes for a bst
+	@Test
+	public void testIsBST() {
+		BST<Integer, String> bst = createBST();
+		
+		assertTrue(bst.isBST());
+		
+		bst.invert();
+		assertFalse(bst.isBST());
+	}
+	
+	//Tests the invert method, which flips the tree to be a mirror image of itself
+	@Test
+	public void testInvert() {
+		BST<Integer, String> bst = createBST();
+		
+		String originalInOrder = bst.inOrder();
+		String originalReverse = bst.reverseOrder();
+		
+		bst.invert();
+
+		String invertedInOrder = bst.inOrder();
+		String invertedReverse = bst.reverseOrder();
+		
+		assertEquals(originalInOrder, invertedReverse);
+		assertEquals(originalReverse, invertedInOrder);
+	}
+	
+	//Tests pre-order traversal of the tree
 	@Test
 	public void testPreOrder() {
-		BST<Integer, String> bst = new BST<Integer, String>();
+		BST<Integer, String> bst = createBST();
 		
-		bst.insert(50, "Fifty");
-		bst.insert(20, "Twenty");
-		bst.insert(10, "Ten");	
-		bst.insert(30, "Thirty");
-		bst.insert(70, "Seventy");
-		bst.insert(60, "Sixty");
-		bst.insert(80, "Eighty");
+		String traversal = bst.preOrder();
+		String correct = "50:Fifty 30:Thirty 20:Twenty 40:Fourty 70:Seventy 60:Sixty 80:Eighty";
+		assertEquals(correct, traversal);
 		
-		System.out.print("\nPre-Order: ");
-		bst.preOrder();
+		bst.clear();
+		assertEquals("", bst.preOrder());
 	}
 	
+	//Tests in-order traversal of the tree
+	@Test
+	public void testInOrder() {
+		BST<Integer, String> bst = createBST();
+		
+		String traversal = bst.inOrder();
+		String correct = "20:Twenty 30:Thirty 40:Fourty 50:Fifty 60:Sixty 70:Seventy 80:Eighty";
+		assertEquals(correct, traversal);
+		
+		bst.clear();
+		assertEquals("", bst.inOrder());
+	}
+	
+	//Tests post-order traversal of the tree
 	@Test
 	public void testPostOrder() {
-		BST<Integer, String> bst = new BST<Integer, String>();
+		BST<Integer, String> bst = createBST();
 		
-		bst.insert(50, "Fifty");
-		bst.insert(20, "Twenty");
-		bst.insert(10, "Ten");	
-		bst.insert(30, "Thirty");
-		bst.insert(70, "Seventy");
-		bst.insert(60, "Sixty");
-		bst.insert(80, "Eighty");
+		String traversal = bst.postOrder();
+		String correct = "20:Twenty 40:Fourty 30:Thirty 60:Sixty 80:Eighty 70:Seventy 50:Fifty";
+		assertEquals(correct, traversal);
 		
-		System.out.print("\nPost-Order: ");
-		bst.postOrder();
+		bst.clear();
+		assertEquals("", bst.postOrder());
 	}
-	*/
+	
+	//Tests reverse order traversal of the tree
+	@Test
+	public void testReverseOrder() {
+		BST<Integer, String> bst = createBST();
+		
+		String traversal = bst.reverseOrder();
+		String correct = "80:Eighty 70:Seventy 60:Sixty 50:Fifty 40:Fourty 30:Thirty 20:Twenty";
+		assertEquals(correct, traversal);
+		
+		bst.clear();
+		assertEquals("", bst.reverseOrder());
+	}
+	
+	//Tests the various constructors of the binary search tree
+	@Test
+	public void testConstructors() {
+		BST<Integer, String> bst1 = new BST<Integer, String>();
+		
+		assertNull(bst1.getRoot());
+		assertEquals(0, bst1.size());
+		
+		BST<Integer, String> bst2 = new BST<Integer, String>(10, "Ten");
+		
+		assertNotNull(bst2.getRoot());	
+		assertTrue(10 == bst2.getRoot().key);
+		assertTrue("Ten" == bst2.getRoot().val);
+		assertEquals(1, bst2.size());
+		
+		HashMap<Integer, String> hm = new HashMap<Integer, String>();
+		hm.put(50, "Fifty");
+		hm.put(20, "Twenty");
+		hm.put(70, "Seventy");
+		hm.put(60, "Sixty");
+		
+		BST<Integer, String> bst3 = new BST<Integer, String>(hm);
+		
+		assertNotNull(bst3.getRoot());
+		assertEquals(4, bst3.size());
+		assertTrue(bst3.containsKey(50));
+		assertTrue(bst3.containsValue("Fifty"));
+		assertTrue(bst3.containsKey(20));
+		assertTrue(bst3.containsValue("Twenty"));
+		assertTrue(bst3.containsKey(70));
+		assertTrue(bst3.containsValue("Seventy"));
+		assertTrue(bst3.containsKey(60));
+		assertTrue(bst3.containsValue("Sixty"));
+		
+		System.out.println(bst3.inOrder());
+	}
 }
